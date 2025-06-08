e7c495e1d5d72c014f3ee1114a746bf6393f19fa
Leandro Meireles <leandromeireles98@gmail.com>
2025-06-08 16:39:36 -0300

    Version 3.0.0 QR INTEGRATION - Added QR code generation, instance management, frontend ready

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-
-const PORT = 8000
-
-app.get('/', (req, res) => res.send('API Working'))
-
-app.listen(PORT, () => console.log('Running'))
